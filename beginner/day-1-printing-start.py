1ce53b305e9ae663c236ccb0553e518be6e87579
carmsanchezs <carm.sanchezs@gmail.com>
2024-01-08 12:44:16 -0600

    Day 1. Variables and project 1

---
+++ beginner/day-1-printing-start.py
@@ -0,0 +1,3 @@
+# input() will get user input in console
+# tehn print() will print the word 'Hello' and the user input
+print("Hello " + input("What is your name? "))
---
+++ beginner/day-1-variables-start.py
@@ -0,0 +1,15 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# Instead of having an understanding line of code like
+print(len(input("What is your name? ")))
+
+# We can use variables and make a more clear code
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+# make your code readable 
