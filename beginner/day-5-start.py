3cf96f7cb5dcd6a61a86755162e8c2f4dbb3ef45
carmsanchezs <carm.sanchezs@gmail.com>
2024-01-08 19:38:44 -0600

    Day 5. lists and Project 5 password generator

---
+++ beginner/day-5-start.py
@@ -0,0 +1,7 @@
+fruits = ["Apple", "Strawberry", "Banana", "Orange"]
+for fruit in fruits:
+    # It executes inside the loop
+    print(fruit)
+    print(f"I want to do a {fruit} pie.")
+# It executes outside the loop
+print(fruits)
