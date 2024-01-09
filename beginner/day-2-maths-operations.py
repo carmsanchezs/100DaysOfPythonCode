67341ec65b648c4322f66fa22e3254ee113e4502
carmsanchezs <carm.sanchezs@gmail.com>
2024-01-08 18:59:34 -0600

    Day 2. Operators and project 2

---
+++ beginner/day-2-maths-operations.py
@@ -0,0 +1,32 @@
+# PEMDASLR
+# ()
+# **
+# * / 
+# + - 
+
+# and the calculation goes from left to right
+print(3 * (3 + 3) / 3 - 3)
+
+print(8 / 3)
+
+print(round(8 / 3))
+
+print(round(8 / 3, 2))
+
+print(8 // 3) # floor division
+
+
+result = 4 / 2
+result /= 2
+print(result)
+
+# a common use
+score = 0
+height = 1.8
+is_winning = True
+
+# in other part of the code
+score += 1
+
+# F - string 
+f"your scores is {score}, your height is {height}, your are winning is {is_winning}"
---
+++ beginner/day-2-start.py
@@ -0,0 +1,27 @@
+# Data Types
+# String
+print("Hello"[4])
+
+print("123" + "456")
+
+# Integer
+print(23 + 345)
+
+a = 342_654_896
+
+# Float
+x = 3141.59
+
+# Boolean
+True
+False
+
+num_char = len(input("What is your name?"))
+
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " character")
+
+a = float(123)
+print(70 + float("100.5"))
+
+print(str(70) + str(100))
