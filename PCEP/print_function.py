b427a67f8e6f377e20a4ab4e8ff954ce0f38db61
carmsanchezs <carm.sanchezs@gmail.com>
2024-01-17 00:11:32 -0600

    Exercises for exam Certified Entry Level Python Programmer

---
+++ PCEP/print_function.py
@@ -0,0 +1,6 @@
+print("My name is", "Python.", end="")
+print("Monty Python")
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="$")
+print("Monty", "Python.", sep="*", end="*\n")
